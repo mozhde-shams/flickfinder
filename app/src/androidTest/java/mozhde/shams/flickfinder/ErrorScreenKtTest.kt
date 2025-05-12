a91ec6a1c1d208ae80df2ca7ea50ea508f7d1bb0
mozhde-shams <mozhde.shams@gmail.com>
2025-05-12 15:21:17 +0330

    Fix errorScreen tests

--- app/src/androidTest/java/mozhde/shams/flickfinder/ErrorScreenKtTest.kt
+++ app/src/androidTest/java/mozhde/shams/flickfinder/ErrorScreenKtTest.kt
@@ -1,10 +1,14 @@
 package mozhde.shams.flickfinder
 
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import mozhde.shams.features.home.ui.ErrorItem
+import mozhde.shams.features.home.ui.ErrorScreen
+import mozhde.shams.features.home.ui.HomeEvent
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -13,13 +17,56 @@ import org.junit.runner.RunWith
 class ErrorScreenKtTest {
 
     @get:Rule
-    val composeTestRule = createAndroidComposeRule<MainActivity>()
+    val composeTestRule = createComposeRule()
 
     @Test
-    fun screenItemsVisibilityTest() {
-        composeTestRule.onNodeWithContentDescription("Seems like something went wrong.")
+    fun errorScreen_displaysErrorMessageAndRetryButton() {
+        var retryClicked = false
+        val dispatch: (HomeEvent) -> Unit = {
+            retryClicked = true
+        }
+        composeTestRule.setContent {
+            ErrorScreen(
+                dispatch = dispatch,
+            )
+        }
+
+        composeTestRule
+            .onNodeWithContentDescription("Seems like something went wrong.")
             .assertIsDisplayed()
-        composeTestRule.onNodeWithText("Seems like something went wrong.").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Try again").assertIsDisplayed()
+        composeTestRule
+            .onNodeWithText("Seems like something went wrong.")
+            .assertIsDisplayed()
+        composeTestRule
+            .onNodeWithText("Try again")
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithText("Try again")
+            .performClick()
+
+        assert(retryClicked)
+    }
+
+    @Test
+    fun errorItem_displayRetryButton() {
+        var retryClicked = false
+        val dispatch: (HomeEvent) -> Unit = {
+            retryClicked = true
+        }
+        composeTestRule.setContent {
+            ErrorItem(
+                dispatch = dispatch,
+            )
+        }
+
+        composeTestRule
+            .onNodeWithText("Try again")
+            .assertIsDisplayed()
+        composeTestRule
+            .onNodeWithText("Try again")
+            .performClick()
+
+        assert(retryClicked)
     }
 }
